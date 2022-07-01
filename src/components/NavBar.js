7f50af34b9ec32db9629ae49948b2c069a2441e0
Evan-Sackmann <100897393+Evan-Sackmann@users.noreply.github.com>
2022-06-30 18:39:14 -0700

    Working Cart Modal

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,19 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
+import Modal from "react-modal";
 import iconLogo from "../images/logo.svg";
 import iconMenu from "../images/icon-menu.svg";
 import iconCart from "../images/icon-cart.svg";
 import iconAvatar from "../images/image-avatar.png";
+import CartModal from "./CartModal";
+import { customStyles } from "../utils/ModalStyling";
 
 export default function NavBar() {
-	const log = () => {
-		console.log("clicked");
-	};
+	const [showModal, setShowModal] = useState(false);
+
+	function openModal() {
+		setShowModal(true);
+	}
+	function closeModal() {
+		setShowModal(false);
+	}
 	return (
 		<nav className="navigation-bar">
-			<img onClick={log} src={iconMenu} alt="" />
-			<img src={iconLogo} alt="" />
-			<img src={iconCart} alt="" />
-			<img src={iconAvatar} alt="" />
+			<div className="navigation-bar-item">
+				<img src={iconMenu} alt="" />
+				<img src={iconLogo} alt="" />
+			</div>
+			<div className="navigation-bar-item">
+				<img
+					onClick={() => setShowModal((prev) => !prev)}
+					src={iconCart}
+					alt=""
+				/>
+				<img className="icon-avatar" src={iconAvatar} alt="" />
+			</div>
+
+			<Modal
+				isOpen={showModal}
+				style={customStyles}
+				shouldCloseOnOverlayClick={true}
+				onRequestClose={closeModal}
+			>
+				<>
+					<p className="">Cart</p>
+					<p className="modal-content">Your cart is empty.</p>
+				</>
+			</Modal>
 		</nav>
 	);
 }
