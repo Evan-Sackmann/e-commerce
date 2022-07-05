3e5ed7e9077e8254ec7160d1246daf92427a75d0
Evan-Sackmann <100897393+Evan-Sackmann@users.noreply.github.com>
2022-07-05 02:45:01 -0700

    Mobile Layout Complete

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,9 +2,10 @@ import React, { useState } from "react";
 import Modal from "react-modal";
 import iconLogo from "../images/logo.svg";
 import iconMenu from "../images/icon-menu.svg";
+import iconDelete from "../images/icon-delete.svg";
 import iconCart from "../images/icon-cart.svg";
+import iconCartImage from "../images/image-product-1-thumbnail.jpg";
 import iconAvatar from "../images/image-avatar.png";
-import CartModal from "./CartModal";
 import { customStyles } from "../utils/ModalStyling";
 import MenuModal from "./MenuModal";
 
@@ -13,9 +14,36 @@ export default function NavBar() {
 	const [showMenuModal, setShowMenuModal] = useState(false);
 
 	function displayCartStatus() {
-		const cartItemCount = localStorage.getItem("itemCount");
-		if (cartItemCount > 0) {
-			return <p>{localStorage.getItem("itemCount")}</p>;
+		const cartCount = localStorage.getItem("itemCount");
+		if (cartCount > 0) {
+			return (
+				<>
+					<div className="cart-content">
+						<img
+							className="cart-image"
+							src={iconCartImage}
+							alt=""
+						/>
+						<div className="cart-content-info">
+							<p>Autumn Limited Edition Sneakers</p>
+							<p>
+								$125.00 x {cartCount}{" "}
+								<span className="cart-total-cost">
+									${localStorage.getItem("itemCount") * 125}
+									.00
+								</span>
+							</p>
+						</div>
+						<img
+							className="cart-delete-icon"
+							onClick={removeFromCart}
+							src={iconDelete}
+							alt="Remove from Cart"
+						/>
+					</div>
+					<button className="cart-checkout">Checkout</button>
+				</>
+			);
 		} else {
 			return <p className="modal-content">Your cart is empty.</p>;
 		}
@@ -32,6 +60,11 @@ export default function NavBar() {
 	}
 	function closeMenuModal() {
 		setShowMenuModal(false);
+	}
+
+	function removeFromCart() {
+		localStorage.removeItem("itemCount");
+		setShowCartModal(false);
 	}
 
 	return (
