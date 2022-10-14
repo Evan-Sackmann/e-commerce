43faa4b6be276665708c6147a10e5356ce0a7dff
Evan-Sackmann <100897393+Evan-Sackmann@users.noreply.github.com>
2022-10-14 13:21:24 -0700

    Checkout page

--- src/components/CheckoutPage.js
+++ src/components/CheckoutPage.js
@@ -1,12 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import data from "../data.json";
 
 export default function CheckoutPage() {
 	let productCount = localStorage.getItem("itemCount");
+
 	return (
 		<div>
 			<Link to="/e-commerce">Back to page</Link>
-			<p>{productCount}</p>
+			<img src="../images/image-product-1-thumbnail.jpg" alt="img" />
+			<p>
+				{data[0].name} x {productCount}
+			</p>
+
+			<button className="cart-checkout">Confirm Purchase</button>
 		</div>
 	);
 }
